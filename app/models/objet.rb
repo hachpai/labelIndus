be82ed20af2635f6bbb7989cd9a7e3580bac3b94
hachpai <wibautph@gmail.com>
2013-05-21 19:38:51 +0200

    S3 URL

--- app/models/objet.rb
+++ app/models/objet.rb
@@ -5,7 +5,7 @@ class Objet < ActiveRecord::Base
 
   @@categories = {"luminaires" => 1, "assises" => 2, "tables" => 3, "horloges" => 4, "autres" => 5, "accueil" => 6}
   #:small => "37x55>", :thumbnail =>  "74x110>", :big => "360x540>", :huge => "642x585>"
-  has_attached_file :image, :styles => {:small => "37x", :thumbnail =>  "74x", :crop => "500x500>", :big => "360x", :huge => "642x"}, :processors => [:cropper]
+  has_attached_file :image, :styles => {:small => "37x", :thumbnail =>  "74x", :crop => "500x500>", :big => "360x", :huge => "642x"}, :processors => [:cropper], :url => ':s3_domain_url' 
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   attr_accessible :crop_x, :crop_y, :crop_w, :crop_h
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'], :message => "Le fichier fourni n'est pas une image."
