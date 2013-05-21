fa2380547f42e3ed4b618122143576fd46098d09
hachpai <wibautph@gmail.com>
2013-05-21 21:50:03 +0200

    test urls

--- app/admin/objets.rb
+++ app/admin/objets.rb
@@ -1,46 +1,5 @@
 ActiveAdmin.register Objet do
 
-  controller do
-    
-    after_filter :reprocess_image, :if => :cropping?, :only => :update
-    
-    def cropping?
-      @objet.cropping?
-    end
-    
-    def reprocess_image
-      @objet.crop_image
-    end
-    
-    def create
-      @objet = Objet.new(params[:objet])
-      if @objet.save
-        flash[:notice] = "Objet bien ajout."
-        if params[:objet][:image].blank?
-          redirect_to admin_objet_path(@objet)
-        else
-          render :action => 'crop', :layout => 'admin/layouts/crop_layout'
-        end
-      else
-        render :action => 'new'
-      end
-    end
-    
-    def update
-      @objet = Objet.find(params[:id])
-      if @objet.update_attributes(params[:objet])
-        flash[:notice] = "Objet mis a jour."
-        if params[:objet][:image].blank?
-          redirect_to admin_objet_path(@objet)
-        else
-          render :action => 'crop', :layout => 'admin/layouts/crop_layout'
-        end
-      else
-        render :action => 'edit'
-      end
-    end
-  end
-  
   index do
     column :reference
     column :image do |objet|
